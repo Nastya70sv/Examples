9d75e878ec48f656769f292857d0a21be5f7db03
Anastasia <nastya70sv@mail.ru>
2022-11-16 13:15:59 +0300

    Added HW 2.1

---
+++ HomeWorks_2/HW2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 < 1000 & num1 > 99)
+{
+    int num2 = num1 / 10;
+    int num3 = num2 % 10;
+    Console.WriteLine($"Второе число {num3}");
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
