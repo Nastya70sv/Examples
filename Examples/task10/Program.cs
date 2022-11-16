f41aededc65119d497ef9bae66f9e6e1840f7b9d
Anastasia <nastya70sv@mail.ru>
2022-11-16 10:27:51 +0300

    Added task 10

---
+++ Examples/task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число {number}");
+int digit1 = number/100;
+int digit3 = number%10;
+Console.WriteLine($"Получилось число {digit1}{digit3}");
